741537cc2d22b56c20dbe2295b6c95379aedc6ac
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-07-17 18:32:04 -0700

    added click event function through sibling child element and setting localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,24 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
+  // click event
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+    console.log(text);
+  });
+  function timeTracker () {
+    var a = moment().hour();
+    $(".time-block").each(function () {
+      //need to make loop and conditional statement
+    })
+  }
+})
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +37,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
